04edcc870288e66088aa16ec15e3dd86328533ce
Ilya Seleznev <seleznev.ilya.a@gmail.com>
2024-06-02 20:41:05 +0300

    create database, migrations and connection db

--- cmd/main.go
+++ cmd/main.go
@@ -2,20 +2,51 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/SeleznevIlya/Go_project"
 	"github.com/SeleznevIlya/Go_project/pkg/handler"
 	"github.com/SeleznevIlya/Go_project/pkg/repository"
 	"github.com/SeleznevIlya/Go_project/pkg/service"
+	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	repos := repository.NewRepository()
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variable: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initializing database: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	service := service.NewService(repos)
 	handler := handler.NewHandler(service)
 
 	srv := new(Go_project.Server)
-	if err := srv.Run("8000", handler.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handler.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
